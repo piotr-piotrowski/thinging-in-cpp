bc0f0d3d2850fc04a4715be3b79719543f260101
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-01-14 23:03:00 +0100

    Chapter 2 - Exercise 2

---
+++ ch02/ex_2_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float radius;
+    cout << "Enter a radius: ";
+    cin >> radius;
+    cout << "Area of the circle is: " << 3.14 * radius * radius << endl;
+    return 0;
+}
